ab6441f0047d95d4da5bf2e9be69d9db867dcdbb
Caspar Kielwein <c.kielwein@bretgeld-engineering.de>
2024-03-29 09:59:39 +0100

    integratio files Aaargh and Ancrestries into skribilo

---
+++ Ancestries.scm
@@ -0,0 +1,194 @@
+(chapter :title [Ancestry]
+(section :title [Dwarf][
+
+Stout and stubborn, Warriors and Craftsmen, Kings and Queens of the Mountain Halls.
+
+Quirks:
+,(enumerate (item [Epic Beard])
+            (item [Stone colored skin])
+            (item [A beard that is meticulously braided and adorned with small metal trinkets and beads.])
+            (item [A bald head with a tattooed pattern of runes and symbols.])
+            (item [Extremely bushy eyebrows that are constantly furrowed in a scowl.])
+            (item [One or more missing fingers, replaced with intricately crafted metal prosthetics.])
+            (item [A perpetually grumpy expression, with downturned lips and furrowed brows.]))
+
+,(subsection :title [Traits][
+,(itemize (item [Speed: 25'])
+          (item [Language: Common, Dwarven])
+          (item [Skills: One Tool of your choice]))
+])
+
+,(subsection :title [Discipline: Dwarven Resilience][
+,(subsubsection :title [Lvl 1 Dwarven Resilience][
+On your turn: As a `Minnor Action` you can choose to spend one point of *Endurance* to regain hit points. ])
+,(subsubsection :title [ Lvl 2: Tough as Nails][
+When you use your Dwarven Resilience, you gain *Damage Resistance* until the end of your next turn.])
+,(subsubsection :title [ Lvl 2: Strength of the Earth][
+When you use your Dwarven Resilience, you can save against all conditions you currently suffer.])
+
+])])
+(section :title [Draken][
+
+Dragonborn, Half-Dragons, The ancient rulers of the skies.
+
+Quirks
+,(enumerate (item [Multiple Tails: Unlike most Draken who have a single tail, this one has two or even three tails.])
+(item [Bioluminescent Eyes: This Draken's eyes glow with a faint bioluminescence.])
+(item [Horned Crest: Instead of a smooth, scaled head, this Draken has a prominent crest of horns.])
+(item [Vibrant Scales: Their scales are brighter and more colorful than most other Draken.])
+(item [Metallic Scales: This Draken's scales have a metallic sheen, making them almost look like they're made of precious metals.])
+(item [Back Ridge: A spiny ridge on your back]))
+
+,(subsection :title [Traits][
+,(itemize (item [Speed: 30'])
+          (item [Language: Common, Draconic])
+          (item [Skills: Intimidate or Religion]))
+])
+
+,(subsection :title [Draconic Heritage][
+Choose dragon and an ascociated element. Gain ,(it [resistance]) to that element.
+])
+
+,(subsection :title [Discipline: Dragonbreath][
+,(subsubsection :title [Lvl 1: Dragonbreath][
+,(itemize
+            (item [Cost: 1 Power-Point])
+            (item [Minor Action})
+            (item [Cone of 15' length, 45° wide])
+            (item [Attack +Tough])
+            (item [1d6 + Tough <element> damage]))
+])
+,(subsubsection :title [Lvl 2: wide breath][
+Dragonbreath becomes a close blast 5
+])
+,(subsubsection :title [Lvl 2: Focused breath][
+Dragonbreath becomes ranged 10.
+Damage becomes d10
+])
+,(subsubsection :title [Lvl 3: Healing breath][
+Allies in blast heal (how much?)
+])
+,(subsubsection :title [Lvl 3: explosive breath][
+Dragonbreath becomes range burst 1.
+Requires focused breath
+])
+,(subsubsection :title [Lvl 4: Powerful breath][
+Add one die to the damage dealt by Dragonbreath
+])
+,(subsubsection :title [Lvl 4: Burning Breath][
+Targets hit by Dragonbreath suffer 10 Ongoing Damage of the chosen elemental type.
+])
+])
+])
+(section :title [Minotaur][
+
+Horned and Dangerous, Large and in Charge
+
+Quirks
+,(enumerate (item [A prominent nose ring or other facial jewelry])
+            (item [Unusually large horns that curve in a distinctive pattern])
+            (item [Black-striped fur])
+            (item [Horn adornments - Your horns are covered in intricate carvings])
+            (item [TODO])
+            (item [TODO]))
+
+,(subsection :title [Traits][
+,(itemize (item [Speed: 30'])
+          (item [Language: Common, Giant or Dwarven])
+          (item [Skills: Nature or Intimidate]))
+])
+
+,(subsection :title [Discipline:  Horns][
+,(subsubsection :title [Lvl 1: Gorge][
+* Tough Melee Attack
+* 1d8 + Tough
+])
+,(subsubsection :title [Lvl 2: Trampling Charge][
+Tough Damage to passed enemies on charge
+])
+,(subsubsection :title [Lvl 3: Piercing Charge][
+Target is grappled on hit
+])
+,(subsubsection :title [Lvl 3: Bleeding Wound][
+Ongoing Damage on hit
+])
+,(subsubsection :title [Terrifying Charge][
+* on hit: secondary attack
+* Attack +Mind against `Will`
+* target is terrified until the end of your next turn.
+* targets evey enemy within 15'
+])
+])
+])
+(section :title [Human][
+
+Quirks
+,(enumerate (item [A noticeable limp])
+            (item [Extravagant hair])
+            (item [Magnificent moustach])
+            (item [Physical prime - an extremely athletic build like a statue of a hero of old])
+            (item [Albino])
+            (item [Overweight, but not necessarily slow or weak]))
+
+,(subsection :title [Traits][
+,(itemize (item [Speed: 30'])
+          (item [Language: Common, once of your choice])
+          (item [Skills: Streetwise or Charm]))
+])
+
+,(subsection :title [Discipline: Human Ambition][
+    ,(subsubsection :title [Lvl 1: Ambition][
+        The hero may spend a *Power-Point* on their turn to gain an additional Combat Action.
+        This ability may only be used once per turn.
+    ])
+])
+])
+(section :title [Elves][
+
+Quirks
+,(enumerate (item [Hair that changes color based on mood or the time of day, ranging from pale silver to deep green.])
+            (item [Thin, almost translucent skin that has an ethereal quality and is prone to showing veins and blood vessels.])
+            (item [Impossibly beautiful])
+            (item [Silver Hair, not just the color - it's literally silver])
+            (item [A tendency to sway or dance to music only you can hear or even to the rhythm of the wind in the trees.])
+            (item [Your eyes are just black orbs])
+            (item [A delicate, almost imperceptible shimmer to your skin that makes you seem like you are always surrounded by a faint aura of magic.])
+            (item [A natural, floral scent that emanates from their skin and hair.]))
+
+,(subsection :title [Traits][
+,(itemize (item [Speed: 30'])
+          (item [Language: Elven])
+          (item [Skills: Arcane, Charm, or Nature]))
+])
+
+,(subsection :title [Accuracy][
+    ,(subsubsection :title [Lvl 1: Accuracy][
+        After making a attack role, you can spend a Power-Point to reroll a single die or both dice when rolling with advantage or disadvantage.
+    ])
+])
+])
+(section :title [Halfling][
+
+Quirks
+,(enumerate (item [Abnormally curly hair that never stays in place.])
+            (item [A perpetually cheerrful expression, even in the face of danger or adversity.])
+            (item [Large pointy ears])
+            (item [Hairy feet and a refusal to wear shoes.])
+            (item [Large eyes making you look cute or spooky])
+            (item [No body hair at all]))
+
+
+,(subsection :title [Traits][
+,(itemize (item [Speed: 30'])
+          (item [Language: once of your choice])
+          (item [Skills: Streetwise or Stealth]))
+])
+
+,(subsection :title [Discipline: Halfling Luck][
+    ,(subsubsection :title [Lvl 1: Luck][
+        After an enemy hits you with an attack, but before damage is rolled, you can spend a Power-Point
+        to force the DM to reroll the attack against you.
+    ])
+])
+])
+)
---
+++ book.scm
@@ -0,0 +1,7 @@
+(use-modules (skribilo package web-book2))
+
+(document :title [Aaargh!] 
+    (include "Aaargh.scm")
+    (include "Ancestries.scm")
+
+)
